e7edf4debb848ae7b3b5d3b690461589aed451fa
cicimen <egemengulkilik@gmail.com>
2021-07-06 23:43:45 +0300

    core model and extension folders add

---
+++ Sources/Core/Extensions/RelatedDigitalDate.swift
@@ -0,0 +1,16 @@
+//
+//  RelatedDigitalDate.swift
+//  
+//
+//  Created by Egemen Gulkilik on 6.07.2021.
+//
+
+import Foundation
+
+extension Date {
+    static private let dateFormatter = DateFormatter()
+    func format(_ format: String = "yyyy-MM-dd HH:mm:ss") -> String {
+        Date.dateFormatter.dateFormat = format
+        return Date.dateFormatter.string(from: self)
+    }
+}
