eca7dae92abb23458a87e89757308163367bebb3
cicimen <egemengulkilik@gmail.com>
2021-07-06 21:51:20 +0300

    RelatedDigitalCookie struct add

---
+++ Sources/Core/Model/RelatedDigitalCookie.swift
@@ -0,0 +1,17 @@
+//
+//  RelatedDigitalCookie.swift
+//  
+//
+//  Created by Egemen Gulkilik on 6.07.2021.
+//
+
+import Foundation
+
+struct RelatedDigitalCookie {
+    var loggerCookieKey: String?
+    var loggerCookieValue: String?
+    var realTimeCookieKey: String?
+    var realTimeCookieValue: String?
+    var loggerOM3rdCookieValue: String?
+    var realTimeOM3rdCookieValue: String?
+}
