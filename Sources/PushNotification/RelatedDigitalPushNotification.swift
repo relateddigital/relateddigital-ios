1955221c9d11d93bf0572c54e4fb5a799a211e18
cicimen <egemengulkilik@gmail.com>
2021-07-12 21:24:56 +0300

    pushnotification folder add

---
+++ Sources/PushNotification/RelatedDigitalPushNotification.swift
@@ -0,0 +1,18 @@
+//
+//  RelatedDigitalPushNotification.swift
+//  RelatedDigitalIOS
+//
+//  Created by Egemen Gulkilik on 10.07.2021.
+//
+
+import Foundation
+
+class RelatedDigitalPushNotification {
+    
+    let relatedDigitalProfile: RelatedDigitalProfile
+    
+    init(relatedDigitalProfile: RelatedDigitalProfile) {
+        self.relatedDigitalProfile = relatedDigitalProfile
+    }
+    
+}
