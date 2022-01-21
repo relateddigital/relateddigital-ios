1ea8d207aa629c85a803bc5ea17aa20b6d5bf509
cicimen <egemengulkilik@gmail.com>
2022-01-22 00:36:12 +0300

    deleted all files

--- README.md
+++
@@ -1,2 +0,0 @@
-# relateddigital-ios
-relateddigital-ios
--- Sources/TargetingAction/RelatedDigitalTargetingAction.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RelatedDigitalTargetingAction.swift
-//  RelatedDigitalIOS
-//
-//  Created by Egemen Gulkilik on 12.07.2021.
-//
-
-import Foundation
