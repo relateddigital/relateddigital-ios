640e56063a0e28e76dc7c38c4b2a1d89b05322c6
orhun <akmilorhun@gmail.com>
2022-05-05 23:14:34 +0300

    config page to page

--- Sources/TargetingAction/InAppNotification/bannerView/BannerViewController.swift
+++ Sources/TargetingAction/InAppNotification/bannerView/BannerViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-public class BannerViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+public class BannerViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
 
     var globalBannerView : bannerView?
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     public override func loadView() {
@@ -24,37 +23,54 @@ public class BannerViewController: UIViewController,UICollectionViewDelegate,UIC
         self.view = view
         let bannerView : bannerView = UIView.fromNib()
         bannerView.translatesAutoresizingMaskIntoConstraints = false
-//        self.view.addSubview(bannerView)
-//        NSLayoutConstraint.activate([bannerView.topAnchor.constraint(equalTo: self.view.topAnchor),
-//                                     bannerView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-//                                     bannerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-//                                     bannerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)])
-//        globalBannerView = bannerView
+        self.view.addSubview(bannerView)
+        NSLayoutConstraint.activate([bannerView.topAnchor.constraint(equalTo: self.view.topAnchor),
+                                     bannerView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+                                     bannerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                                     bannerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)])
+        globalBannerView = bannerView
         
-        
-        self.willMove(toParent: self)
+        //For Workable Delegate
+        self.willMove(toParent: addedController)
         self.view.frame = view.bounds
         view.addSubview(bannerView)
         addedController.addChild(self)
-        self.didMove(toParent: self)
+        self.didMove(toParent: addedController)
+        //
         
-        
+        bannerView.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "bannerCell")
+
         bannerView.collectionView.dataSource = self
         bannerView.collectionView.delegate = self
+        bannerView.collectionView.contentInset = UIEdgeInsets(top: 2, left: 0, bottom: 2, right: 0)
 
     }
     
     
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print("selam")
         return 10
     }
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell : bannerCollectionViewCell = UIView.fromNib()
-        cell.imageView.backgroundColor = .blue
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "bannerCell", for: indexPath)
+        cell.contentMode = .center
         
+        let cellTemp : bannerCollectionViewCell = UIView.fromNib()
+        cellTemp.imageView.backgroundColor = .yellow
+        cell.addSubview(cellTemp)
+        cellTemp.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([cellTemp.topAnchor.constraint(equalTo: cell.topAnchor),
+                                     cellTemp.leadingAnchor.constraint(equalTo: cell.leadingAnchor),
+                                     cellTemp.trailingAnchor.constraint(equalTo: cell.trailingAnchor),
+                                     cellTemp.bottomAnchor.constraint(equalTo: cell.bottomAnchor)])
+
+
         return cell
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.width, height: self.view.height)
     }
     
     required init?(coder: NSCoder) {
