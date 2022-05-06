c21e47af0edf302e68fcb9ac25446e5a66724301
orhun <akmilorhun@gmail.com>
2022-05-06 21:35:05 +0300

    xib config file

--- Sources/TargetingAction/InAppNotification/bannerView/bannerView.swift
+++ Sources/TargetingAction/InAppNotification/bannerView/bannerView.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class bannerView: UIView {
 
+    @IBOutlet weak var currentPageView: UIView!
+    @IBOutlet weak var currentPageLabel: UILabel!
+    @IBOutlet weak var pageControlView: UIPageControl!
+    @IBOutlet weak var pageControlHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var collectionView: UICollectionView!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
