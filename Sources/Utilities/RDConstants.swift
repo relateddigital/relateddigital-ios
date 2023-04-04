400f91b22954cf664c68b6d980fbd6baf0119566
orhun <akmilorhun@gmail.com>
2023-04-04 14:48:00 +0300

    version update

--- Sources/Utilities/RDConstants.swift
+++ Sources/Utilities/RDConstants.swift
@@ -35,7 +35,7 @@ public class urlConstant {
 
 struct RDConstants {
     
-    static let sdkVersion = "4.0.13"
+    static let sdkVersion = "4.0.14"
     
     static let HTTP = "http"
     static let HTTPS = urlConstant.shared.securityTag
