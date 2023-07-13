5015f77f21cfead445b23449fb3c032d6f01c79d
Umut Can Alparslan <umut_alparslan@hotmail.com>
2023-07-13 16:14:00 +0300

    update swiping

--- Sources/Utilities/RDConstants.swift
+++ Sources/Utilities/RDConstants.swift
@@ -9,10 +9,10 @@ import UIKit
 
 public class UrlConstant {
     public static var shared = UrlConstant()
-    var urlPrefix = "s.visilabs.net"
-    var securityTag = "https"
-    public var organizationId = "676D325830564761676D453D"
-    public var profileId = "356467332F6533766975593D"
+    var urlPrefix = "tests.visilabs.net"
+    var securityTag = "http"
+    public var organizationId = "394A48556A2F76466136733D"
+    public var profileId = "75763259366A3345686E303D"
     
     public func setTest() {
         urlPrefix = "tests.visilabs.net"
@@ -486,7 +486,7 @@ struct RDConstants {
     static let spintoWinUrl = "https://mbls.visilabs.net/spin_to_win.js"
     static let findToWinUrl = "https://mbls.visilabs.net/find_to_win.js"
     static let giftBoxUrl = "https://mbls.visilabs.net/giftbox.js"
-    static let chooseFavoriteUrl = "https://mbls.visilabs.net/giftbox.js"
+    static let chooseFavoriteUrl = "https://mbls.visilabs.net/swiping.js"
 
     
     
