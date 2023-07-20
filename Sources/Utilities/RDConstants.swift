23aca789bb9941c3856f54ab0d596eecac7ec0a1
Umut Can Alparslan <umut_alparslan@hotmail.com>
2023-07-20 10:09:44 +0300

    sek version update

--- Sources/Utilities/RDConstants.swift
+++ Sources/Utilities/RDConstants.swift
@@ -35,7 +35,7 @@ public class UrlConstant {
 
 struct RDConstants {
     
-    static let sdkVersion = "4.0.24"
+    static let sdkVersion = "4.0.25"
     
     static let HTTP = "http"
     static let HTTPS = UrlConstant.shared.securityTag
