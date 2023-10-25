57fb1e6abaede1a8adfe59c1412dbc7db3fa72a3
Umut Can Alparslan <umut_alparslan@hotmail.com>
2023-10-25 14:03:32 +0300

    mini inapp bug fix

--- Sources/Utilities/RDConstants.swift
+++ Sources/Utilities/RDConstants.swift
@@ -35,7 +35,7 @@ public class UrlConstant {
 
 struct RDConstants {
     
-    static let sdkVersion = "4.0.31"
+    static let sdkVersion = "4.0.32"
     static let sdkType = "native"
     
     static let HTTP = "http"
