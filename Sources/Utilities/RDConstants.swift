370cad1418f95acc510ccd2372b0ab6d931d9696
orhun <akmilorhun@gmail.com>
2024-09-30 13:10:32 +0300

    version update

--- Sources/Utilities/RDConstants.swift
+++ Sources/Utilities/RDConstants.swift
@@ -35,7 +35,7 @@ public class UrlConstant {
 
 struct RDConstants {
     
-    static let sdkVersion = "4.0.43"
+    static let sdkVersion = "4.0.44"
     static let sdkType = "native"
     
     static let HTTP = "http"
