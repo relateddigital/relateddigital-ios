bf0fecc3f229a136b10653f9cf2b1d457c706c6f
Umut Can Alparslan <umut_alparslan@hotmail.com>
2024-12-10 14:06:54 +0300

    pod update

--- Sources/Utilities/RDConstants.swift
+++ Sources/Utilities/RDConstants.swift
@@ -35,7 +35,7 @@ public class UrlConstant {
 
 struct RDConstants {
     
-    static let sdkVersion = "4.0.48"
+    static let sdkVersion = "4.0.49"
     static let sdkType = "native"
     
     static let HTTP = "http"
