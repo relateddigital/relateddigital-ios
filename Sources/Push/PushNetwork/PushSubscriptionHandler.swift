a03effaa7a426891093118c918d60c7d016352a3
orhun <akmilorhun@gmail.com>
2025-02-27 10:48:36 +0300

    ww crash read write lock

--- Sources/Push/PushNetwork/PushSubscriptionHandler.swift
+++ Sources/Push/PushNetwork/PushSubscriptionHandler.swift
@@ -29,38 +29,58 @@ class PushSubscriptionHandler {
             return
         }
         
-        var isRequestSame = false
-        var isRequestSameAsLastSuccessfulSubscriptionRequest = false
-        self.readWriteLock.read {
-            if subscriptionRequest == inProgressSubscriptionRequest {
-                isRequestSame = true
+        crashCapturer()
+        
+        DispatchQueue.main.async { [self] in
+            var isRequestSame = false
+            var isRequestSameAsLastSuccessfulSubscriptionRequest = false
+            self.readWriteLock.read {
+                if subscriptionRequest == inProgressSubscriptionRequest {
+                    isRequestSame = true
+                }
+                if let lastSuccessfulSubscriptionRequest = PushUserDefaultsUtils.getLastSuccessfulSubscription()
+                    , PushUserDefaultsUtils.getLastSuccessfulSubscriptionTime().addingTimeInterval(TimeInterval(PushKey.threeDaysInSeconds)) > Date()
+                    , lastSuccessfulSubscriptionRequest == subscriptionRequest {
+                    isRequestSameAsLastSuccessfulSubscriptionRequest = true
+                }
             }
-            if let lastSuccessfulSubscriptionRequest = PushUserDefaultsUtils.getLastSuccessfulSubscription()
-                , PushUserDefaultsUtils.getLastSuccessfulSubscriptionTime().addingTimeInterval(TimeInterval(PushKey.threeDaysInSeconds)) > Date()
-                , lastSuccessfulSubscriptionRequest == subscriptionRequest {
-                isRequestSameAsLastSuccessfulSubscriptionRequest = true
+                    
+            if isRequestSame {
+                RDLogger.info("EMSubscriptionHandler request is not valid. EMSubscriptionRequest is the same as the previous one.")
+                return
             }
+            
+            if isRequestSameAsLastSuccessfulSubscriptionRequest {
+                RDLogger.info("EMSubscriptionHandler request is not valid. EMSubscriptionRequest is the same as the lastSuccessfulSubscription.")
+                return
+            }
+            
+            self.readWriteLock.write {
+                self.inProgressSubscriptionRequest = subscriptionRequest
+                RDLogger.info("EMSubscriptionHandler reportSubscription: \(subscriptionRequest.encoded)")
+            }
+            
+            push.pushAPI?.request(requestModel: subscriptionRequest, retry: 3, completion: self.subscriptionRequestHandler)
+            _ = semaphore.wait(timeout: DispatchTime.distantFuture)
         }
-                
-        if isRequestSame {
-            RDLogger.info("EMSubscriptionHandler request is not valid. EMSubscriptionRequest is the same as the previous one.")
-            return
-        }
-        
-        if isRequestSameAsLastSuccessfulSubscriptionRequest {
-            RDLogger.info("EMSubscriptionHandler request is not valid. EMSubscriptionRequest is the same as the lastSuccessfulSubscription.")
-            return
-        }
-        
-        self.readWriteLock.write {
-            self.inProgressSubscriptionRequest = subscriptionRequest
-            RDLogger.info("EMSubscriptionHandler reportSubscription: \(subscriptionRequest.encoded)")
-        }
-        
-        push.pushAPI?.request(requestModel: subscriptionRequest, retry: 3, completion: self.subscriptionRequestHandler)
-        _ = semaphore.wait(timeout: DispatchTime.distantFuture)
         
     }
+    
+    
+    private func crashCapturer() {
+        do {
+            // Yalancı ve güvenli işlem
+            let result = try? DispatchTime(uptimeNanoseconds: 32)
+            // Başarılı işlemden sonra yapılacaklar
+            print("RD crash Capturer Config")
+            
+        } catch {
+            // Herhangi bir hata oluştuğunda yapılacaklar
+            print("RD crash capturer done")
+        }
+    }
+    
+    
     
     private func subscriptionRequestHandler(result: Result<PushResponse?, PushAPIError>) {
         switch result {
