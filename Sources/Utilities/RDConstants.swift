5509df044d8d1d422a1c24952b064c2b93738706
Umut Can Alparslan <umut_alparslan@hotmail.com>
2025-05-16 14:46:03 +0300

    version update

--- Sources/Utilities/RDConstants.swift
+++ Sources/Utilities/RDConstants.swift
@@ -40,7 +40,7 @@ struct LogConfig: Codable {
 
 struct RDConstants {
     
-    static let sdkVersion = "4.0.67"
+    static let sdkVersion = "4.0.68"
     static let sdkType = "native"
     
     static let HTTP = "http"
